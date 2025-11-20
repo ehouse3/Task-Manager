7305d1a090707f89278810aea5b6083650a61001
ehouse3 <houseesh@gmail.com>
2025-11-20 15:54:16 -0500

    user registration filtering

--- nextjs-frontend/taskmanager/app/register/page.tsx
+++ nextjs-frontend/taskmanager/app/register/page.tsx
@@ -3,9 +3,33 @@
 import { Button, TextField, PasswordField } from "@/lib/components";
 import { register } from "@/lib/api/auth";
 import { AuthResponse, RegisterRequest } from "@/lib/types/auth";
-import { FormEvent, FormEventHandler } from "react";
+import { FormEvent, FormEventHandler, useState } from "react";
 
 export default function UserRegister() {
+  const [result, setResult] = useState<string>(""); // result of registration
+
+  function isValidUsername(username: string): boolean {
+    if (username.length < 1) {
+      return false;
+    }
+    return true;
+  }
+  function isValidEmail(email: string): boolean {
+    if (email.length < 1) {
+      return false;
+    }
+    if (!email.includes("@")) {
+      return false;
+    }
+    return true;
+  }
+  function isValidPassword(password: string): boolean {
+    if (password.length < 1) {
+      return false;
+    }
+    return true;
+  }
+
   // Handler for submitting form. Generates reigstration request for new user and calls api
   const handleSubmit: FormEventHandler<HTMLFormElement> = (
     event: FormEvent<HTMLFormElement>
@@ -17,18 +41,29 @@ export default function UserRegister() {
       email: formData.get("email")?.toString() ?? "",
       password: formData.get("password")?.toString() ?? "",
     };
-    const response: Promise<AuthResponse> = register(request);
-    console.log(response);
+
+    try {
+      if (!isValidUsername(request.username)) { throw new Error("invalid username"); }
+      if (!isValidEmail(request.email)) { throw new Error("invalid email"); }
+      if (!isValidPassword(request.password)) { throw new Error("invalid password"); }
+
+      const response: Promise<AuthResponse> = register(request);
+      console.log(response);
+    } catch (error) {
+      setResult("Invalid arguments");
+      console.log(error);
+    }
   };
 
   return (
     <div className="flex flex-col">
-      <h1>User Registration</h1>
-      <form className="flex flex-col w-sm" onSubmit={handleSubmit}>
+      <form className="flex flex-col w-sm items-center  " onSubmit={handleSubmit}>
+        <h1>User Registration</h1>
         <TextField placeHolder="Username" required={true} name="username" />
         <TextField placeHolder="Email" required={true} name="email" />
         <PasswordField placeHolder="Password" required={true} name="password" />
         <Button innerText="Create" type="submit" />
+        <h3 className="text-red-800">{result}</h3>
       </form>
     </div>
   );

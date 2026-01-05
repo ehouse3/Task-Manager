54faefa19ac295c1861090243881c51dbe685601
ehouse3 <houseesh@gmail.com>
2026-01-05 12:28:42 -0500

    Display projects
    Displays list of projects for user

--- nextjs-frontend/taskmanager/app/[user]/page.tsx
+++ nextjs-frontend/taskmanager/app/[user]/page.tsx
@@ -1,66 +1,69 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { User } from "@/lib/types/user";
 import { useAuth } from "../auth/AuthContext";
 import { createProject } from "@/lib/api/projects";
 import { CreateProjectDto, Project } from "@/lib/types/project";
+import { getProjectsByUserId } from "@/lib/api/users";
 
-export default function UserDashboard({ params }: { params: Promise<{ user: User }> }) {
-
+export default function UserDashboard({
+  params,
+}: {
+  params: Promise<{ user: User }>;
+}) {
   // const { user } = use(params); // dont need params because user is defined in auth context
   const auth = useAuth();
 
-  useEffect(() => {
-    if (!auth || !auth.user) {
-      return;
-    }
-    fetchProjects(auth.user.id);
-  });
-
   // Wait for auth to be initialized
-  if (auth?.isLoading) { // improve
+  if (auth?.isLoading) {
+    // improve
     return <div>Loading...</div>;
   }
 
-  // if (!auth || !auth.user) {
-  //   throw new Error("Auth or user not present");
-  // }
-  // const user: User = auth.user;
+  console.log(auth?.user);
+  console.log(auth?.user?.projects);
 
-  function fetchProjects(userId: number) {
-    console.log("Fetching projects for user", userId);
-    return;
+  interface ProjectsNavigationProps {
+    projects: Project[];
   }
-  // const [users, setUsers] = useState<User[] | null>(null);
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const response: User[] = await getAllUsers();
-
-  //     setUsers(response);
-  //   }
-  //   fetchData();
-  // }, []);
+  function ProjectsNavigation(props: ProjectsNavigationProps): ReactElement {
+    return (
+      <div>
+        <ul>
+          {props.projects.map((project: Project) => (
+            <li key={project.id}>
+              <h2>{project.name}</h2>
+              <h3>{project.description ?? ""}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 
   return (
     <div>
-      {/* <h1>Hello {user.nickname ?? user.username}</h1> */}
+      <h1>Welcome {auth?.user?.nickname ?? auth?.user?.username}</h1>
+
+      {/* Create new project */}
+      <div>
+      
+      </div>
+      
+      {/* List and Navigate to projects */}
+      <div>
+        {auth?.user?.projects ? (
+          <ProjectsNavigation projects={auth.user.projects} />
+        ) : (
+          <h2>No projects found</h2>
+        )}
+      </div>
+
+      {/* Calendar? */}
       <div>
 
       </div>
-      <div>
-        <ul>
-          {}
-        </ul>
-      </div>
-      <ul>
-        {/* {users?.map((user: User) => (
-          <div key={user.id}>
-            <li>username: {user.username}</li>
-            <li>password: {user.email}</li>
-          </div>
-        ))} */}
-      </ul>
     </div>
   );
 }

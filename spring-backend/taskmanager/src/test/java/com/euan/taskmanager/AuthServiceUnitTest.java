bd7840bcead08b6f006b45909b7381a720b8d624
ehouse3 <houseesh@gmail.com>
2026-02-02 14:32:09 -0500

    Testing Setup Fix

---
+++ spring-backend/taskmanager/src/test/java/com/euan/taskmanager/AuthServiceUnitTest.java
@@ -0,0 +1,12 @@
+package com.euan.taskmanager;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/** Unit tests for Authservice with Mockito
+ * No Database Needed
+ */
+@ExtendWith(MockitoExtension.class)
+class AuthServiceUnitTests {
+
+}

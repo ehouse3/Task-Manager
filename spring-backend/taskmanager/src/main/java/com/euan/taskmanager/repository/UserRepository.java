4a184574dd35dfb45e3aa9dd841fea08c3ed805b
ehouse3 <houseesh@gmail.com>
2026-02-20 19:28:08 -0500

    Update UserRepository.java
    Added findByIdWithProjects to retrieve a user by their id, and eagerly fetch the user's projects as well

--- spring-backend/taskmanager/src/main/java/com/euan/taskmanager/repository/UserRepository.java
+++ spring-backend/taskmanager/src/main/java/com/euan/taskmanager/repository/UserRepository.java
@@ -1,14 +1,27 @@
 package com.euan.taskmanager.repository;
 
 import com.euan.taskmanager.model.User;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> { 
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
     Optional<User> findByEmail(String email);
+
     boolean existsByUsername(String username);
+
     boolean existsByEmail(String email);
+
+    // Retrieves a User by their Id, eager fetching projects alongside
+    @EntityGraph(attributePaths = { "projects" })
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> findByIdWithProjects(@Param("id") Long id);
+
 }

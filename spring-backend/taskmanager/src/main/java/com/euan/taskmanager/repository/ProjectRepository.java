a378f0db4524f27e3676635aa40c24bfb80b8934
ehouse3 <houseesh@gmail.com>
2026-02-25 11:13:20 -0500

    Update ProjectRepository.java
    Added findByIdWithTasks function to project repository, for fetching a project and eagerly fetching its tasks

--- spring-backend/taskmanager/src/main/java/com/euan/taskmanager/repository/ProjectRepository.java
+++ spring-backend/taskmanager/src/main/java/com/euan/taskmanager/repository/ProjectRepository.java
@@ -1,10 +1,20 @@
 package com.euan.taskmanager.repository;
 
 import com.euan.taskmanager.model.Project;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     
+    // Retrieves a Project by their Id, eager fetching Tasks alongside
+    @EntityGraph(attributePaths = { "projects" })
+    @Query("SELECT p FROM Project p WHERE p.id = :id")
+    List<Project> findByIdWithTasks(@Param("id") Long id);
 }
